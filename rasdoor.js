8300b1ffc62290d1cb3fcdba014b1424379882bf
Eric Bakan <eric@ebakan.com>
2012-12-03 16:16:07 -0500

    Newer node server

--- rasdoor.js
+++ rasdoor.js
@@ -6,11 +6,13 @@ var port = 9001;
 
 server.listen(port);
 var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("/dev/tty.usbmodem1411");
+var serialport = new SerialPort("/dev/ttyACM0");
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
     serialport.write(String.fromCharCode(179));
+    console.log("Open\n");
     sleep.sleep(2);
     serialport.write(String.fromCharCode(0));
+    console.log("Closed\n");
 });
